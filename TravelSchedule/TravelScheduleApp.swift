7d079523adbdb47b38af9359f8bf1ed3de69b74a
D1mon227 <d1mon227@yandex.ru>
2024-03-03 12:19:03 +0200

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Dmitrii Medvedev on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
